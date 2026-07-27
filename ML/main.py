d6e4a51cdc44ea9423a22d48b25171b687761a7a
NishikaRaj <nishikar299@gmail.com>
2026-07-27 05:13:33 -0700

    Add initial ML main module

---
+++ ML/main.py
@@ -0,0 +1,7 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))  
+print("sum=", num1+num2 )
+print("sub=", num1-num2 )
+
+for i in range (10):
+    print("Shivam")
