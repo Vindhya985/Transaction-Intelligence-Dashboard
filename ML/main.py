7c20fbbf3825631005c9158b0c88e07b37879626
NishikaRaj <nishikar299@gmail.com>
2026-07-28 03:48:41 -0700

    complete data processing script and generate cleaned dataset from the raw one

--- ML/main.py
+++ ML/main.py
@@ -1,7 +1,31 @@
-num1=float(input("Enter first number: "))
-num2=float(input("Enter second number: "))  
-print("sum=", num1+num2 )
-print("sub=", num1-num2 )
+# read file using Pandas
+import pandas as pd
+df=pd.read_csv('sample-100-rows.csv')
+# inspecting info
+print(df.info())
+print(df.head())
 
-for i in range (10):
-    print("Shivam")
+# handling missing values
+print(df.isnull().sum())
+# for example fill missing debit or credit values with 0
+df['Debit'] = df['Debit'].fillna(0.0)
+df['Credit'] = df['Credit'].fillna(0.0)
+# fill the missing descriptions with defaults
+df['Category'] = df['Category'].fillna('Uncategorized')
+df['Description'] = df['Description'].fillna('Unknown')
+# removing duplicates
+# by dropping identical rows
+df = df.drop_duplicates()
+# ensure transaction IDs are unique
+df = df.drop_duplicates(subset=['Transaction_ID'])
+# format the date and amount columns
+df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')
+numeric_columns = ['Debit', 'Credit','Balance']
+for col in numeric_columns:
+    df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0.0)
+# for clean transaction descriptions
+df['Description'] = df['Description'].astype(str).str.strip().str.title()
+df.to_csv('cleaned_sample_100_rows.csv', index=False)
+print("DATA PROCESSING COMPLETED")
+print(df.info())
+print(df.head())
